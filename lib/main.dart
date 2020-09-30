e563772845007d013d38e5e9d42bb86e6b2c4c6e
Ceren <cerenbulbul27@gmail.com>
2020-09-30 13:37:08 -0700

    onBoarding screen 2

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,233 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: OnboardingScreen1(),
+      home: Home(),
     );
   }
 }
 
 
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  List<SliderModel> mySLides = new List<SliderModel>();
+  int slideIndex = 0;
+  PageController controller;
+
+  Widget _buildPageIndicator(bool isCurrentPage){
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 2.0),
+      height: isCurrentPage ? 10.0 : 6.0,
+      width: isCurrentPage ? 10.0 : 6.0,
+      decoration: BoxDecoration(
+        color: isCurrentPage ? Color.fromRGBO(137, 168, 73, 1) : Color.fromRGBO(173, 216, 100, 1),
+        borderRadius: BorderRadius.circular(12),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    mySLides = getSlides();
+    controller = new PageController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [const Color.fromRGBO(27, 64, 67, 1), const Color.fromRGBO(137, 168, 73, 1)])),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          height: MediaQuery.of(context).size.height - 100,
+          child: PageView(
+            controller: controller,
+            onPageChanged: (index) {
+              setState(() {
+                slideIndex = index;
+              });
+            },
+            children: <Widget>[
+              SlideTile(
+                title: mySLides[0].getTitle(),
+                desc: mySLides[0].getDesc(),
+              ),
+              SlideTile(
+                title: mySLides[1].getTitle(),
+                desc: mySLides[1].getDesc(),
+              ),
+              SlideTile(
+                title: mySLides[2].getTitle(),
+                desc: mySLides[2].getDesc(),
+              )
+            ],
+          ),
+        ),
+        bottomSheet: slideIndex != 2 ? Container(
+          margin: EdgeInsets.symmetric(vertical: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              FlatButton(
+                onPressed: (){
+                  controller.animateToPage(2, duration: Duration(milliseconds: 400), curve: Curves.linear);
+                },
+                splashColor: Color.fromRGBO(137, 168, 73, 1),
+                child: Text(
+                  "SKIP",
+                  style: TextStyle(color: Color.fromRGBO(137, 168, 73, 1), fontWeight: FontWeight.w600),
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    for (int i = 0; i < 3 ; i++) i == slideIndex ? _buildPageIndicator(true): _buildPageIndicator(false),
+                  ],),
+              ),
+              FlatButton(
+                onPressed: (){
+                  print("this is slideIndex: $slideIndex");
+                  controller.animateToPage(slideIndex + 1, duration: Duration(milliseconds: 500), curve: Curves.linear);
+                },
+                splashColor: Color.fromRGBO(137, 168, 73, 1),
+                child: Text(
+                  "NEXT",
+                  style: TextStyle(color: Color.fromRGBO(137, 168, 73, 1), fontWeight: FontWeight.w600),
+                ),
+              ),
+            ],
+          ),
+        ): InkWell(
+          onTap: (){
+            print("Get Started Now");
+          },
+          child: Container(
+            height: 70,
+            color: Color.fromRGBO(137, 168, 73, 1),
+            alignment: Alignment.center,
+            child: Text(
+              "GET STARTED NOW",
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SlideTile extends StatelessWidget {
+  String title, desc;
+
+  SlideTile({this.title, this.desc});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('images/logo_fible.png'),
+          SizedBox(
+            height: 40,
+          ),
+          Text(title, textAlign: TextAlign.center,style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 24,
+            color: Color.fromRGBO(137, 168, 73, 1)
+          ),),
+          SizedBox(
+            height: 20,
+          ),
+          Text(desc, textAlign: TextAlign.center,style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 14,
+          color: Color.fromRGBO(27, 64, 67, 1))
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+class SliderModel{
+
+  String imageAssetPath;
+  String title;
+  String desc;
+
+  SliderModel({this.imageAssetPath,this.title,this.desc});
+
+  void setImageAssetPath(String getImageAssetPath){
+    imageAssetPath = getImageAssetPath;
+  }
+
+  void setTitle(String getTitle){
+    title = getTitle;
+  }
+
+  void setDesc(String getDesc){
+    desc = getDesc;
+  }
+
+  String getImageAssetPath(){
+    return imageAssetPath;
+  }
+
+  String getTitle(){
+    return title;
+  }
+
+  String getDesc(){
+    return desc;
+  }
+
+}
+
+
+List<SliderModel> getSlides(){
+
+  List<SliderModel> slides = new List<SliderModel>();
+  SliderModel sliderModel = new SliderModel();
+
+  //1
+  sliderModel.setDesc("Discription");
+  sliderModel.setTitle("Title");
+  slides.add(sliderModel);
+
+  sliderModel = new SliderModel();
+
+  //2
+  sliderModel.setDesc("Description");
+  sliderModel.setTitle("Title");
+  slides.add(sliderModel);
+
+  sliderModel = new SliderModel();
+
+  //3
+  sliderModel.setDesc("Description");
+  sliderModel.setTitle("Title");
+  slides.add(sliderModel);
+
+  sliderModel = new SliderModel();
+
+  return slides;
+}
+
+
+
+//Onboarding 1
 class OnboardingScreen1 extends StatefulWidget {
   @override
   _OnboardingScreenState createState() => _OnboardingScreenState();
