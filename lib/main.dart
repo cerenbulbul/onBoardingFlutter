9605db4a8a29b845abd1956a046e620ed5ea4a5c
Ceren <cerenbulbul27@gmail.com>
2020-10-04 22:46:38 -0700

    Edit Code and Commit

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: OnboardingScreen4(),
+      home: OnboardingScreen1(),
     );
   }
 }
